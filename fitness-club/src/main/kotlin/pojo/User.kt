815728b8b75c10c03883be79e7bfa75ab0c3b5b4
Republicn <bloomashka@mail.ru>
2020-04-06 00:30:33 +0300

    deleted unnecessary pojo, small fix

--- fitness-club/src/main/kotlin/pojo/User.kt
+++
@@ -1,7 +0,0 @@
-package pojo
-
-data class User(val id: Int, val name: String, val subscriptionId: Int?) {
-    init {
-        assert(id > 0) { "Non-positive user id: $id" }
-    }
-}
--- fitness-club/src/main/kotlin/turnstile/command/TurnstileEventHandler.kt
+++ fitness-club/src/main/kotlin/turnstile/command/TurnstileEventHandler.kt
@@ -8,11 +8,11 @@ class TurnstileEventHandler(private val dao: FitnessClubDaoImpl) {
     fun handle(event: TurnstileEvent) =
         when (event) {
             is Enter -> {
-                dao.enterOrExitEvent(event.subscriptionId, event.userId, event.timestamp, "enter")
+                dao.enterOrExitEvent(event.subscriptionId, event.userId, event.timestamp, "ENTER")
                 ReportStorage.enter(event.userId, event.timestamp)
             }
             is Exit -> {
-                dao.enterOrExitEvent(event.subscriptionId, event.userId, event.timestamp, "exit")
+                dao.enterOrExitEvent(event.subscriptionId, event.userId, event.timestamp, "EXIT")
                 ReportStorage.exit(event.userId, event.timestamp)
             }
         }
